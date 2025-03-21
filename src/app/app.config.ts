bc29e502df7c679bfb6a76f3e4e5d3c2f675747e
krish-wappnet <krish.wappnet@gmail.com>
2025-03-21 17:31:23 +0530

    added localstorage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,38 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideStore } from '@ngrx/store';
+import { provideStore, ActionReducer } from '@ngrx/store';
 import { employeesReducer } from './employees/employees.reducer';
 
+// Meta-reducer to persist state to localStorage
+export function persistStateReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    const nextState = reducer(state, action);
+    localStorage.setItem('employeesState', JSON.stringify(nextState)); // Line 21: localStorage is used here
+    return nextState;
+  };
+}
+
+// Meta-reducer to hydrate state from localStorage on app init
+export function hydrateState(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state, action) => {
+    if (state === undefined) {
+      const persisted = localStorage.getItem('employeesState');
+      return persisted ? JSON.parse(persisted) : reducer(state, action);
+    }
+    return reducer(state, action);
+  };
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), 
-    provideClientHydration(withEventReplay()), 
-    provideStore({ employees: employeesReducer })]
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideStore(
+      { employees: employeesReducer },
+      { metaReducers: [hydrateState, persistStateReducer] }
+    )
+  ]
 };
